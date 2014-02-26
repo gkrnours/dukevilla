38a166fd0e126f370e57fbef9f566c54198015dd
Coues Ludovic <couesl@gmail.com>
2014-02-26 14:19:33 +0100

    Alter the layout foundation

--- db/client.js
+++ db/client.js
@@ -1,6 +1,7 @@
 var redis  = require("redis")
 
 var nv = process.env
+console.log(nv.OPENSHIFT_REDIS_PORT)
 var r = new redis.createClient(
 		nv.OPENSHIFT_REDIS_PORT || 6379,
 	 	nv.OPENSHIFT_REDIS_HOST || "127.0.0.1")
